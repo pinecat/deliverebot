b494f181b548b01ad4cb926861fafd85d4f8f717
JoeDivinagracia <joediv789@gmail.com>
2019-11-04 18:43:43 -0500

    Called MenuView from Menu.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MenuView from './Menu'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+	  <Text>Hello World!</Text>
+	  <MenuView />
     </View>
   );
 }
