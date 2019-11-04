7a0f89678ee951d2cb9bec606e9df8bb7bec2c20
JoeDivinagracia <joediv789@gmail.com>
2019-11-04 18:44:01 -0500

    Created Menu.js to hold Jay's Nest menu items

---
+++ Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, View, Text} from 'react-native';
+
+export default class MenuView extends React.Component{
+    constructor(props){
+	super(props);
+    }
+
+    render(){
+	return (
+		<View>
+		<Text title="filler">Gucci Gang, Gucci Gang, Gucci Gang. Something something I did cocaine.</Text>
+		</View>
+	);
+    }
+}
