b366892a9aa9da304a652667e915127d34bfaaa8
rory <rory.dudley@gmail.com>
2019-11-08 17:26:05 -0500

    constructing authurl

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AuthSession, Linking } from 'expo';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+// https://login.microsoftonline.com/1d884f12-a0d7-42f0-8b15-3a91c853bcb5/oauth2/authorize?client_id=1e034920-213a-4862-a8d7-da1bded43317&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A19006
+
+const APP_ID = '1e034920-213a-4862-a8d7-da1bded43317';
+
+export default class App extends React.Component {
+  state = {
+    result: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="Login" onPress={this._handlePressAsync} />
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+        ) : <Text>wut</Text>}
+      </View>
+    );
+  }
+
+  _handlePress() {
+    console.log('test');
+  }
+
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl();
+    //let redirectUrl = 'http://localhost:19006';
+    let result = await AuthSession.startAsync({
+      authUrl:
+        `https://login.microsoftonline.com/1d884f12-a0d7-42f0-8b15-3a91c853bcb5/oauth2/authorize?client_id=1e034920-213a-4862-a8d7-da1bded43317&response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    });
+    this.setState({ result });
+  }
 }
 
 const styles = StyleSheet.create({
