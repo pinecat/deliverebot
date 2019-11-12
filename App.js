53f031568d9fd9551882fc5b0c9945749649f35c
rory <rory.dudley@gmail.com>
2019-11-11 22:03:15 -0500

    same as authsession branch, but using my own created library 'azure-ad-graph-expo'@1.1.0

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AuthSession, Linking } from 'expo';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { openAuthSession } from 'azure-ad-graph-expo';
 
 // https://login.microsoftonline.com/1d884f12-a0d7-42f0-8b15-3a91c853bcb5/oauth2/authorize?client_id=1e034920-213a-4862-a8d7-da1bded43317&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A19006
 
@@ -9,89 +10,31 @@ const APP_ID = '1e034920-213a-4862-a8d7-da1bded43317';
 export default class App extends React.Component {
   state = {
     result: null,
-    tokenResponse: null,
-    responseJson: null,
   };
 
   render() {
     return (
       <View style={styles.container}>
         <Button title="Login" onPress={this._handlePressAsync} />
-        {this.state.responseJson ? (
-          <Text>{JSON.stringify(this.state.responseJson)}</Text>
-        ) : <Text>wut</Text>}
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result)}</Text>
+        ) : <Text>Nothing to see here.</Text>}
       </View>
     );
   }
 
-  _handlePress() {
-    console.log('test');
-  }
-
   _handlePressAsync = async () => {
-    let redirectUrl = AuthSession.getRedirectUrl();
-    //let redirectUrl = 'http://localhost:19006';
-    let result = await AuthSession.startAsync({
-      authUrl:
-        `https://login.microsoftonline.com/1d884f12-a0d7-42f0-8b15-3a91c853bcb5/oauth2/authorize?client_id=1e034920-213a-4862-a8d7-da1bded43317&response_type=code&redirect_uri=${encodeURIComponent(redirectUrl)}`,
-    });
+    let result = await openAuthSession(azureAdAppProps);
     this.setState({ result });
-    console.log(result.params.code);
-    this.getToken(result.params.code, redirectUrl);
   }
+}
 
-  getToken = async (code, redirectUrl) => {
-    var requestParams = {
-      client_id: '1e034920-213a-4862-a8d7-da1bded43317',
-      scope: 'user.read',
-      code: code,
-      redirect_uri: redirectUrl,
-      grant_type: 'authorization_code',
-      client_secret: '/?/v@JHWsa3JyWATGB0k6o9?qaNBk.fN',
-    };
-
-    var formBody = [];
-    for (var p in requestParams) {
-      var encodedKey = encodeURIComponent(p);
-      var encodedValue = encodeURIComponent(requestParams[p]);
-      formBody.push(encodedKey + '=' + encodedValue);
-    }
-    formBody = formBody.join('&');
-
-    fetch(`https://login.microsoftonline.com/1d884f12-a0d7-42f0-8b15-3a91c853bcb5/oauth2/v2.0/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-      },
-      body: formBody,
-    })
-    .then((response) => response.json())
-    .then((tokenResponse) => {
-      console.log(tokenResponse);
-      this.callMsGraph(tokenResponse.access_token);
-      this.setState({ tokenResponse });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  }
-
-  callMsGraph(token) {
-    fetch('https://graph.microsoft.com/v1.0/me', {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + token,
-      }
-    })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log(responseJson);
-      this.setState({ responseJson })
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  }
+const azureAdAppProps = {
+  clientId: '1e034920-213a-4862-a8d7-da1bded43317',
+  tenantId: '1d884f12-a0d7-42f0-8b15-3a91c853bcb5',
+  scope: 'user.read',
+  redirectUrl: AuthSession.getRedirectUrl(),
+  clientSecret: '/?/v@JHWsa3JyWATGB0k6o9?qaNBk.fN',
 }
 
 const styles = StyleSheet.create({
