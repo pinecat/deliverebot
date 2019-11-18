13e015acfa137b7afbf838ddca4cee30aa085305
JoeDivinagracia <joediv789@gmail.com>
2019-11-18 15:31:25 -0500

    Don't think I modified anything in this file. It just told me to add and commit this file.

--- styles.js
+++ styles.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
@@ -31,7 +31,6 @@ export const styles = StyleSheet.create({
       marginTop: 10,
       marginBottom: 10,
       backgroundColor: '#1784ea',
-      width: '100%',
       //backgroundColor: '#062037'
     },
 });
