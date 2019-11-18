3e1e946a3a1129cbb050d48f5cea7982ea62b905
rory <rory.dudley@gmail.com>
2019-11-18 11:05:56 -0500

    fixing App.js

--- App.js
+++ App.js
@@ -1,16 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import AuthView from './AuthView'
-
-export default function App() {
-  return (
-        <View style={styles.container}>
-            <Text>Hello World!</Text>
-            <AuthView />
-        </View>
-    );
-=======
 import Login from './views/Login';
 
 export default class App extends React.Component {
@@ -19,5 +7,4 @@ export default class App extends React.Component {
       <Login />
     );
   }
->>>>>>> 5a3ce7d26097cc8b886cfce8a84c72d3e32d46a3
 }
